8b084a1c6f029bc5a0ebc31f3a0336955b75ff9a
Shah <rifat_rifats@live.com>
2018-05-03 05:32:13 +0600

    fix methods (L5.6)
    - fix methods (L5.6)

--- src/Baum/Console/BaumCommand.php
+++ src/Baum/Console/BaumCommand.php
@@ -25,7 +25,7 @@ class BaumCommand extends Command {
    *
    * @return void
    */
-  public function fire() {
+  public function handle() {
       $this->line('<info>Baum</info> version <comment>' . Baum::VERSION . '</comment>');
       $this->line('A Nested Set pattern implementation for the Eloquent ORM.');
       $this->line('<comment>Copyright (c) 2013 Estanislau Trepat</comment>');
